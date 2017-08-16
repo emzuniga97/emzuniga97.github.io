52f2f8eda55449d1a67dbfdd5ce0446bebf323a2
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-16 16:51:42 -0700

    background recolor

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,3 +1,3 @@
 function colorSwap () {
-  document.getElementById("home").background-color = "rgb(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255))";
+  document.getElementById("home").style.backgroundColor = rgb(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255));
 }
