cefe4f8d8c0095029b72a4b2d05c87bd13a6b84d
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-16 17:17:35 -0700

    background recolor

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,5 +1,12 @@
 function colorSwap () {
-  document.getElementById("home").style.backgroundColor = "rgb(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255))";
+  var r = Math.floor(Math.random()*255);
+  console.log(r);
+  var g = Math.floor(Math.random()*255);
+  console.log(g);
+  var b = Math.floor(Math.random()*255);
+  console.log(b);
+
+  document.getElementById("home").style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 }
 
 /* rgb(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255)); */
