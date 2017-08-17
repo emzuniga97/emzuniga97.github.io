82b5ac32ca5ed8c3b161de4b8de4fc5edf2ee8a2
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-17 12:21:13 -0700

    animation speedup

--- public/js/picado.js
+++ public/js/picado.js
@@ -1,46 +1,47 @@
 var drops = [];
 
-function Drop (rgb, loc){ // prototype for droplets
-    this.core = new Path.Circle({
-      center: loc,
-      radius: 10,
-      fillColor: {hue: rgb, saturation: 1, brightness: 1},
-      strokeColor: {hue: rgb, saturation: 1, brightness: 1}
-    })
-    this.core.applyMatrix = false;
+function Drop (rgb, loc){
+  this.core = new Path.Circle({
+    center: loc,
+    radius: 10,
+    fillColor: {hue: rgb, saturation: 1, brightness: 1},
+    strokeColor: {hue: rgb, saturation: 1, brightness: 1}
+  })
+  this.core.applyMatrix = false;
 }
 
 Drop.prototype = {
 
-    grow: function(factor, limit, peers) { // ripples out drop
+    grow: function(factor, limit, peers) {
+       // console.log(this.core.scaling.x);
+        //console.log(limit);
 
-       if (this.core.scaling.x < limit){
-          var speedup = 1 + peers / 100;
-          //console.log(speedup);
-          this.core.scale(factor * speedup);
-       }
+        if (this.core.scaling.x < limit){
+            var speedup = 1 + peers / 100;
+            console.log(speedup);
+            this.core.scale(factor * speedup);
+        }
 
-       if (this.core.opacity > 0.001){
-           this.core.opacity *= .99;
-       } else {
-           this.core.opacity = 0;
-       }
+        if (this.core.opacity > 0.001){
+           this.core.opacity -= 0.01;
+        }
     }
 }
 
 function onMouseUp(event){
-    var colorHue = Math.random() * 360; 
+    var colorHue = Math.random() * 360;
+    //console.log(colorHue);
     drops.push(new Drop(colorHue, event.point));
 }
 
 function onFrame(event){
     for (var i = 0; i < drops.length; i++){
 
-       single = drops[i];
-       single.grow(1.04,25, drops.length);
-
-       if(single.core.opacity == 0){
-         drops.splice(i, 1); // remove old drops
-       }
+        single = drops[i];
+        single.grow(1.04,25, drops.length);
+        if(single.core.opacity < 0.001){
+            single.core.opacity = 0;
+            drops.splice(i, 1);
+        }
     }
 }
