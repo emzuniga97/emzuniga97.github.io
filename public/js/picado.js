276847aec420d7cec7d62cd9992e36041ea950d0
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-16 22:38:35 -0700

    add proto drops

--- public/js/picado.js
+++ public/js/picado.js
@@ -1,9 +1,22 @@
-var path = new Path.Rectangle({
+var square = new Path.Rectangle({
   point: [75,75],
   size: [75, 75],
   strokeColor: 'red'
 });
 
+var protoDrop = new Path.Circle({
+  center: [0,0],
+  radius: 10,
+  fillColor: 'black',
+  strokeColor: 'black'
+});
+
+var drop = new Symbol(protoDrop);
+
+function onMouseClick(event){
+  drop.place(event.point);
+}
+
 function onFrame(event){
-  path.rotate(3);
+
 }
