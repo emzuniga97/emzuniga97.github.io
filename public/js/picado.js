622934f28fec0c2e7b615f60d00e7123f6f48525
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-16 23:01:47 -0700

    fixed onClick

--- public/js/picado.js
+++ public/js/picado.js
@@ -9,6 +9,8 @@ var drop = new Symbol(protoDrop);
 
 function onClick(event){
   var single = drop.place(event.point);
+  console.log(event.point.x);
+  console.log(event.point.y);
 }
 
 function onFrame(event){
