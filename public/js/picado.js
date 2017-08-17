07490a91d5ac9d315b0f21ede6e99dce866eed94
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-17 10:22:14 -0700

    change to prototypes from symbols

--- public/js/picado.js
+++ public/js/picado.js
@@ -1,34 +1,44 @@
-var protoDrop = new Path.Circle({
-  center: [0,0],
-  radius: 10,
-  fillColor: 'black',
-  strokeColor: 'black'
-});
+// var protoDrop = new Path.Circle({
+//   center: [0,0],
+//   radius: 10,
+//   fillColor: 'black',
+//   strokeColor: 'black'
+// });
 
-var drop = new Symbol(protoDrop);
+// var drop = new Symbol(protoDrop);
+var drops = [];
+
+function Drop (rgb, loc){
+  this.core = new Path.Circle({
+    center: loc,
+    radius: 10,
+    fillColor.hue: rgb,
+    strokeColor.hue: rgb
+  })
+}
 
 function onMouseUp(event){
-  drop.definition.fillColor.hue = Math.random() * 360;
-  var single = drop.place(event.point);
+//  drop.definition.fillColor.hue = Math.random() * 360;
+//  var single = drop.place(event.point);
+  var color = Math.random() * 360;
+  droplets.push(new Drop(color, event.point));
   console.log(event.point.x);
   console.log(event.point.y);
 }
 
-var children = project.activeLayer.children;
-
 function onFrame(event){
-  for (var i = 0; i < children.length; i++){
-    child = children[i];
+  for (var i = 0; i < drops.length; i++){
+    single = drops[i];
 
-    if (child.scaling.x < 30){
-      child.scale(1.04);
+    if (single.core.radius < 30){
+      single.core.radius *= 1.04;
     }
 
-    if (child.opacity > 0.001){
-      child.opacity *= 0.99;
-    } else {
-      child.opacity = 0;
-    }
+    // if (child.opacity > 0.001){
+    //   child.opacity *= 0.99;
+    // } else {
+    //   child.opacity = 0;
+    // }
 
   }
 }
