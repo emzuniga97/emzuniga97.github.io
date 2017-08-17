392f8edf62126e9d6b80094c52aaa22798b4278f
emzuniga97 <Efrain@Efrains-MacBook-Air-4.local>
2017-08-16 23:07:02 -0700

    fixed onClick

--- public/js/picado.js
+++ public/js/picado.js
@@ -7,12 +7,8 @@ var protoDrop = new Path.Circle({
 
 var drop = new Symbol(protoDrop);
 
-function onClick(event){
+function onMouseUp(event){
   var single = drop.place(event.point);
   console.log(event.point.x);
   console.log(event.point.y);
-}
-
-function onFrame(event){
-
 }
